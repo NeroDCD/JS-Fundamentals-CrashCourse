2dee8d03c4304fc1cb12f973c570e3038ca18e65
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-09-04 23:39:45 +0800

    Basic Commands, TextContent

---
+++ 01.The Basics/index.js
@@ -0,0 +1,19 @@
+// console.log(`Hello World`);
+// console.log(`I Like Figma`);
+
+// window.alert(`This is an Alert!`);
+// window.alert(`I Like Framer`);
+
+// Change text content of HTML Content > select element > change the textContent > set the text Ex(Hello World)
+document.getElementById("myHeading").textContent = `Hello World`;
+
+document.getElementById("myParagraph").textContent = `I Like Figma`;
+
+// This is a comment
+
+/*
+This
+is 
+a
+comment
+*/
