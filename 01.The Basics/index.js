8b9217781fa50b7fa0bc6c575f52edac28fc9c54
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-09-09 00:47:37 +0800

    Introduction

--- 01.The Basics/index.js
+++ 01.The Basics/index.js
@@ -1,30 +1,31 @@
 // 01. JavaScript tutorial for beginners
+
 // Console Log
-// Output "Hello World" to the console
+// 💬 Output "Hello World" to the console
 // console.log(`Hello World`);
 
-// Output "I Like Figma" to the console
+// 💬 Output "I Like Figma" to the console
 // console.log(`I Like Figma`);
 
 // Alert Boxes
-// Display an alert box with the message "This is an Alert!"
+// 🚨 Display an alert box with the message "This is an Alert!"
 // window.alert(`This is an Alert!`);
 
-// Display an alert box with the message "I Like Framer"
+// 🚨 Display an alert box with the message "I Like Framer"
 // window.alert(`I Like Framer`);
 
 // Change Text Content in HTML
-// Select the HTML element with ID "myHeading" and set its text content to "Hello World"
+// ✏️ Select the HTML element with ID "myHeading" and set its text content to "Hello World"
 document.getElementById("myHeading").textContent = `Hello World`;
 
-// Select the HTML element with ID "myParagraph" and set its text content to "I Like Figma"
+// ✏️ Select the HTML element with ID "myParagraph" and set its text content to "I Like Figma"
 document.getElementById("myParagraph").textContent = `I Like Figma`;
 
 // Single-line Comment
-// This is a comment
+// 🗨️ This is a comment
 
 /*
-Multi-line Comment
+🗨️ Multi-line Comment
 This
 is
 a
