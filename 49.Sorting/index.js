d04a6f43d18242607427b2effb7596f7a67f6f77
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-09-23 23:22:18 +0800

    Add files via upload

---
+++ 49.Sorting/index.js
@@ -0,0 +1,15 @@
+// 49. Learn JavaScript SORTING
+
+// 🔡 sort() = method used to sort elements of an array in place.
+// Sorts elements as strings in lexicographic order, not alphabetical.
+// ℹ️ Lexicographic = (alphabet + numbers + symbols) as strings.
+
+// 📖 Lexicographic refers to the order in which words or strings are arranged in a dictionary-like sequence.
+// It’s based on the alphabetical order of their component characters,
+// comparing one character at a time from left to right.
+
+//Example
+
+// 🧠 Simple Analogy 🧠
+
+// 🎯 Possible Use Cases:
