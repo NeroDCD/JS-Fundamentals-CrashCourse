a809d0802cf53a65909913204e170f21a5ea2c15
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-09-27 20:45:11 +0800

    Add files via upload

---
+++ 50.Shuffle/index.js
@@ -0,0 +1,12 @@
+// 50. How to SHUFFLE AN ARRAY
+
+// 🔀 The Fisher–Yates shuffle is an algorithm for randomly shuffling a list.
+// It works by going through the list and swapping each element with a randomly chosen one,
+// ensuring every possible order is equally likely. The modern version is efficient,
+// shuffling the list in place with a time complexity proportional to the list size.
+
+//Example
+
+// 🧠 Simple Analogy 🧠
+
+// 🎯 Possible Use Cases:
