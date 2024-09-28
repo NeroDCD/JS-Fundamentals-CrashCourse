ad84c9250d252e129f90f99f2b060fdc9f95f0a5
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-09-28 19:11:21 +0800

    Add files via upload

---
+++ 51.Date Objects/index.js
@@ -0,0 +1,15 @@
+// 51. Learn JavaScript DATE objects
+
+// 🗓️ A Date object in JavaScript is used to work with dates and times.
+// It represents a specific point in time and provides methods to manipulate and format dates.
+
+// 🔑 Key Uses:
+// ⏰ Get the current date and time.
+// 📅 Set or retrieve specific components (year, month, day, hour, etc.).
+// ➗ Perform calculations with dates (e.g., find the difference between two dates).
+
+//Example
+
+// 🧠 Simple Analogy 🧠
+
+// 🎯 Possible Use Cases:
