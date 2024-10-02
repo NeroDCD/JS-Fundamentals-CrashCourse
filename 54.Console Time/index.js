f647eb7563f00a6656f9aea4eec599885b9b341a
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-10-02 15:31:26 +0800

    Add files via upload

---
+++ 54.Console Time/index.js
@@ -0,0 +1,11 @@
+// 54. JavaScript console.time()
+
+// ⏱️ console.time() = tool that allows you to measure the time it takes
+// for a section of code or process to execute.
+// 🔍 Great for identifying performance "bottlenecks."
+
+//Example
+
+// 🧠 Simple Analogy 🧠
+
+// 🎯 Possible Use Cases:
