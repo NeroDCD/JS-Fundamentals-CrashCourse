92f2a1df2ab68bf61fbfeb091c829f9a468bb8cb
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-10-03 17:36:53 +0800

    Add files via upload

---
+++ 55.Format Currency/index.js
@@ -0,0 +1,18 @@
+// 55. Format currency in JavaScript
+
+// 🌍 .toLocaleString() = returns a string with a language
+// sensitive representation of a number. This means the format of the number
+// will adjust based on the specified locale and options, such as using commas
+// or periods for thousands and decimal separators.
+
+// 🔧 Syntax: .toLocaleString("locale", {options});
+// - "locale": Specifies the language and region (e.g., "en-US" for
+//   American English or "de-DE" for German).
+// - {options}: An object that lets you customize the formatting,
+//   like specifying minimum/maximum fraction digits or currency display.
+
+//Example
+
+// 🧠 Simple Analogy 🧠
+
+// 🎯 Possible Use Cases:
