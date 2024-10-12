441716f3bf6ce833e3fa9c797d714eca99562064
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-10-12 19:37:57 +0800

    Random Number Generator

--- 09.Random Number Generator/index.js
+++ 09.Random Number Generator/index.js
@@ -49,13 +49,13 @@ myButton.onclick = function () {
 // 5️⃣ The click event triggers random number generation and updates the text in the labels.
 
 // 🎯 Possible Use Cases:
-// 1️⃣ Game development: Generating random values for dice rolls, card shuffling, or random events. 🎲
-// 2️⃣ User interfaces: Creating dynamic elements, such as randomized color themes or layout adjustments. 🎨
-// 3️⃣ Data sampling: Selecting random samples from a dataset for testing or analysis. 📊
-// 4️⃣ Lottery systems: Picking random winners or entries in a raffle or contest. 🎟️
-// 5️⃣ Testing and simulations: Generating random data for testing algorithms or simulating various scenarios. 🔍
-// 6️⃣ Random password generation: Creating secure, random passwords for user accounts. 🔐
-// 7️⃣ Generating random user IDs or tokens in applications for unique identification. 🆔
-// 8️⃣ Creating random story elements or characters in creative writing or interactive fiction. 📚
-// 9️⃣ Simulating random events or behaviors in virtual environments or simulations. 🌐
-// 🔟 Designing randomized quizzes or games where questions or challenges vary each time. 🎮
+// 1️⃣ Game development 🎲: Generating random values for dice rolls, card shuffling, or random events.
+// 2️⃣ User interfaces 🎨: Creating dynamic elements, such as randomized color themes or layout adjustments.
+// 3️⃣ Data sampling 📊: Selecting random samples from a dataset for testing or analysis.
+// 4️⃣ Lottery systems 🎟️: Picking random winners or entries in a raffle or contest.
+// 5️⃣ Testing and simulations 🔍: Generating random data for testing algorithms or simulating various scenarios.
+// 6️⃣ Random password generation 🔐: Creating secure, random passwords for user accounts.
+// 7️⃣ User identification 🆔: Generating random user IDs or tokens in applications for unique identification.
+// 8️⃣ Creative writing 📚: Creating random story elements or characters in interactive fiction.
+// 9️⃣ Virtual simulations 🌐: Simulating random events or behaviors in virtual environments.
+// 🔟 Randomized quizzes 🎮: Designing games where questions or challenges vary each time.
