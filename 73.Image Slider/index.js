21d424ed7af4e1375a9aa8d61a59bc65b05aa5f6
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-21 21:22:44 +0800

    a

--- 73.Image Slider/index.js
+++ 73.Image Slider/index.js
@@ -8,6 +8,39 @@
 
 // 1️⃣ Example 1 (Title Here)
 // Explain this, add emoji also
+const slides = document.querySelectorAll(".slider img");
+let slideIndex = 0;
+let intervalId = null;
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
 // 🧠 Simple Analogy 🧠
 //Explain this, add emoji also
