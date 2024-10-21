d8214265e8a2e21f414a54aa743f2d1b345314de
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-21 20:58:18 +0800

    Start

---
+++ 73.Image Slider/index.js
@@ -0,0 +1,18 @@
+// 73. Build a JavaScript IMAGE SLIDER
+
+// 🖼️ This program implements an image slider for beginners.
+// 🔍 Element selectors are used to create a NodeList of images to cycle through.
+// 🎨 The displayed image will have a class added to its classList that contains display: block.
+// 🖼️ At least a few images are needed for this project.
+// ⚖️ It is recommended to use images that are roughly the same size.
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+// [emoji here]: description
+
+// 🎯 Possible Use Cases:
+//10 Use Cases, add emoji also
+// 1️⃣ Use cases 1 [emoji here]: description
