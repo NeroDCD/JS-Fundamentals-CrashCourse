1cbd9f6935de09f251dce66dd703278f5d35723b
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-21 18:38:13 +0800

    a

--- 72.Rock Paper Scissors/index.js
+++ 72.Rock Paper Scissors/index.js
@@ -7,6 +7,36 @@
 // 1️⃣ Example 1 (Title Here)
 // Explain this, add emoji also
 
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!🏆";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "You Win!🏆" : "You Lose😭";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "You Win!🏆" : "You Lose😭";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "You Win!🏆" : "You Lose😭";
+        break;
+    }
+  }
+  playerDisplay.textContent = `😎 Player: ${playerChoice}`;
+  computerDisplay.textContent = `💻 Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
+  //   console.log(computerChoice);
+}
+
 // 🧠 Simple Analogy 🧠
 //Explain this, add emoji also
 // [emoji here]: description
