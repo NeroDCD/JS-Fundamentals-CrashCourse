a5d4e2cce84ea1396a17973f29b2664967d7e609
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-25 01:57:21 +0800

    Cookies

---
+++ 78.Cookies/index.js
@@ -0,0 +1,14 @@
+// 78. Learn how JavaScript COOKIES work!
+
+// 🍪 Cookie = A small text file stored on the user's computer.
+// 📝 It is used to remember information about the user, such as preferences or login status.
+// 🔗 Cookies are saved in name=value pairs and can be accessed by the web application.
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
