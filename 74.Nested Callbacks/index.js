2b2a4a9b45014303649dc19353aa05f12ebeaed8
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-24 16:40:02 +0800

    Coding

--- 74.Nested Callbacks/index.js
+++ 74.Nested Callbacks/index.js
@@ -8,6 +8,44 @@
 // 1️⃣ Example 1 (Title Here)
 // Explain this, add emoji also
 
+function task1(callback) {
+  setTimeout(() => {
+    console.log("Task 1 Complete");
+    callback();
+  }, 2000);
+}
+function task2(callback) {
+  setTimeout(() => {
+    console.log("Task 2 Complete");
+    callback();
+  }, 1000);
+}
+function task3(callback) {
+  setTimeout(() => {
+    console.log("Task 3 Complete");
+    callback();
+  }, 3000);
+}
+function task4(callback) {
+  setTimeout(() => {
+    console.log("Task 4 Complete");
+    callback();
+  }, 1500);
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {
+                console.log("All Task Complete");
+            })
+        })
+    })
+});
+
+
+
+
 // 🧠 Simple Analogy 🧠
 //Explain this, add emoji also
 
