8808a7f57a25288aef05b688046cc501208c80c2
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-25 02:30:43 +0800

    Fetch

---
+++ 79.Fetch/index.js
@@ -0,0 +1,16 @@
+// 79. How to FETCH data from an API using JavaScript
+
+// 🌐 Fetch = A function used for making HTTP requests to retrieve resources.
+// 📄 It can fetch various types of data, such as JSON, images, and files.
+// 🚀 Fetch simplifies asynchronous data fetching in JavaScript,
+// 🔗 allowing interaction with APIs to retrieve and send data asynchronously over the web.
+// 📝 Syntax: fetch(url, {options})
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
