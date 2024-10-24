7058b5ba6d28ed5287a79d443faa335ad2de03e9
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-24 17:32:34 +0800

    Coding

--- 76.Async and Await/index.js
+++ 76.Async and Await/index.js
@@ -11,6 +11,64 @@
 // 1️⃣ Example 1 (Title Here)
 // Explain this, add emoji also
 
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogWalked = true;
+      if (dogWalked) {
+        resolve("You walk the dog 🐕");
+      } else {
+        reject("You didn't walk the dog❌");
+      }
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const kitchenCleaned = true;
+      if (kitchenCleaned) {
+        resolve("You clean the kitchen 🧹🧼🧽");
+      } else {
+        reject("You didn't clean the kitchen❌");
+      }
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const trashTakenOut = true;
+      if (trashTakenOut) {
+        resolve("You take out the trash 🗑️");
+      } else {
+        reject("You didn't take out the trash❌");
+      }
+    }, 500);
+  });
+}
+
+async function doChores() {
+  try {
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
+
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+
+    const trashTakenResult = await takeOutTrash();
+    console.log(trashTakenResult);
+
+    console.log("You finished all the chores!🏁");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+doChores();
+
 // 🧠 Simple Analogy 🧠
 //Explain this, add emoji also
 
