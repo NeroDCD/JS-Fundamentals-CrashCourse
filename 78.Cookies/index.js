d313daa66dc1cbc357dff110f29ce4794d756a50
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-25 02:23:15 +0800

    Cookies

--- 78.Cookies/index.js
+++ 78.Cookies/index.js
@@ -24,6 +24,16 @@ const lastText = document.querySelector("#lastText");
 const submitBtn = document.querySelector("#submitBtn");
 const cookieBtn = document.querySelector("#cookieBtn");
 
+submitBtn.addEventListener("click", () => {
+  setCookie("firstName", firstText.value, 365);
+  setCookie("lastName", lastText.value, 365);
+});
+
+cookieBtn.addEventListener("click", () => {
+  firstText.value = getCookie("firstName");
+  lastText.value = getCookie("lastName");
+});
+
 function setCookie(name, value, daysToLive) {
   const date = new Date();
   date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
