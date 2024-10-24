bfeb0ee8be67559ce3bd92cd93bff714b39e4b26
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-24 17:24:03 +0800

    Async/Await

---
+++ 76.Async and Await/index.js
@@ -0,0 +1,18 @@
+// 76. JavaScript ASYNC/AWAIT is easy
+
+// 🌟 Async/Await = A way to handle asynchronous operations in JavaScript.
+// 🚀 Async = A keyword that makes a function return a Promise automatically.
+// ⏳ Await = A keyword that makes an async function wait for a Promise to resolve before continuing execution.
+// ✍️ This allows you to write asynchronous code in a more synchronous manner, improving readability.
+
+// 📜 Note: Async functions do not have `resolve` or `reject` parameters like traditional Promises.
+// 📜 Everything after the `await` keyword is placed in an event queue, ensuring it executes after the Promise is settled.
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
