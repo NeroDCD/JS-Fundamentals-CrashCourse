0c19d6366c303714d6e80ba84059b25f1588f707
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-24 16:28:55 +0800

    Setting up

---
+++ 74.Nested Callbacks/index.js
@@ -0,0 +1,15 @@
+// 74. What is JavaScript NESTED CALLBACK
+
+// 🌀 Nested Callback = A situation in JavaScript where callbacks are nested within other callbacks.
+// 📚 This leads to code that is difficult to read and maintain.
+// ⏳ It was an old pattern used to handle asynchronous functions.
+// 🚀 To avoid nested callbacks, use Promises or async/await for cleaner and more manageable code.
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
