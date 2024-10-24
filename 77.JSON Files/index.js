d6b03a7749dbe36cbb30f2e1beff9dea932b6122
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-24 17:59:15 +0800

    JSON Files

---
+++ 77.JSON Files/index.js
@@ -0,0 +1,17 @@
+// 77. Learn JSON files
+
+// 📄 JSON = JavaScript Object Notation, a data-interchange format.
+// 🌐 It is commonly used for exchanging data between a server and a web application.
+// 🗃️ JSON files can be structured as {key: value} pairs or as arrays [value1, value2, value3].
+
+// 🔄 JSON.stringify() = A method that converts a JavaScript object into a JSON string.
+// 🔄 JSON.parse() = A method that converts a JSON string back into a JavaScript object.
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
