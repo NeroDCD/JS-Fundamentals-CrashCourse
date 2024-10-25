272d24c6fb9ed0845dba2e0f1f51254c18b409bf
Nero <93488370+NeroDCD@users.noreply.github.com>
2024-10-25 14:19:16 +0800

    Weather App

--- 80.Weather App/index.js
+++ 80.Weather App/index.js
@@ -1,9 +1,51 @@
-// 00. Title
+// 80. Build a JavaScript WEATHER APP
 
-// Explanation of the topic, add emoji also
+// 🌦️ Sample Project: Learn and practice how to fetch data from an API and display it.
+// 🔗 This project uses OpenWeather's API for accessing weather data.
+// 📄 API Documentation:
+// - General Info: https://openweathermap.org/
+// - Current Weather: https://openweathermap.org/current
+// - Additional Weather Data: https://openweathermap.org/weather-co...
 
 // 1️⃣ Example 1 (Title Here)
 // Explain this, add emoji also
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "6c5da548c9f7a380af45a39648d59c17";
+
+weatherForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
+  } else {
+    displayError("Please enter a city");
+  }
+});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function getWeatherEmoji(weatherId) {}
+
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
+}
 
 // 🧠 Simple Analogy 🧠
 //Explain this, add emoji also
