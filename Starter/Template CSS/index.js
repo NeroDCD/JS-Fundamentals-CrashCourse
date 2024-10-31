e3dde44bf46cad0d49060714f5e9e8acdea70da6
Nero 📀 <93488370+NeroDCD@users.noreply.github.com>
2024-11-01 06:09:45 +0800

    Add files via upload

---
+++ Starter/Template CSS/index.js
@@ -0,0 +1,14 @@
+// 00. Title
+
+// Explanation of the topic, add emoji also
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+// [emoji here]: description
+
+// 🎯 Possible Use Cases:
+//10 Use Cases, add emoji also
+// 1️⃣ Use cases 1 [emoji here]: description
---
+++ Starter/Template JS/index.js
@@ -0,0 +1,12 @@
+// 00. Title
+
+// Explanation of the topic
+
+// 1️⃣ Example 1 (Title Here)
+// Explain this, add emoji also
+
+// 🧠 Simple Analogy 🧠
+//Explain this, add emoji also
+
+// 🎯 Possible Use Cases:
+//10 Use Cases
